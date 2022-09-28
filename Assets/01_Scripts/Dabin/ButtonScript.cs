40c18a8b26eac8dc913c581a0d1405e5fd3f1797
dabin0519 <dadabin0519@naver.com>
2022-09-28 19:20:44 +0900

    수정겸 작업중

---
+++ Assets/01_Scripts/Dabin/ButtonScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject door1;
+
+    private Animator animD; // door애니메이터
+    private Animator anim;  // button 애니메이터
+    private bool doorOpen;
+
+    private void Start()
+    {
+        animD = GameObject.Find("Door").GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isButtonD", true);
+            doorOpen = true; // *수정사항 : 애니메이션 종료후 doorOpen = true 로 변경;
+        }
+
+        if (doorOpen)
+        {
+            ButtonDown();
+            doorOpen = false;
+        }
+    }
+
+    public void ButtonDown()
+    {
+        animD.SetBool("isButtonDown", true);
+    }
+}
